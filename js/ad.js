6e3799d1f7302b5608d0858f79dc95f2c4dedb05
Your NameHamilton <hamilton@hts-net.com.br>
2023-04-14 21:13:05 -0300

    ad

---
+++ js/ad.js
@@ -0,0 +1,9 @@
+
+atOptions = {
+    'key': '87a0b0107c646108dca671ff143edece',
+    'format': 'iframe',
+    'height': 90,
+    'width': 728,
+    'params': {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/87a0b0107c646108dca671ff143edece/invoke.js"></scr' + 'ipt>');
