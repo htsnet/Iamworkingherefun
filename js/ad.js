9df36b570b3a6e02a2f40d5d690869ed37c95fb2
Your NameHamilton <hamilton@hts-net.com.br>
2023-04-14 21:36:18 -0300

    ad after onload

--- js/ad.js
+++ js/ad.js
@@ -1,8 +1,10 @@
-atOptions = {
-    'key': '87a0b0107c646108dca671ff143edece',
-    'format': 'iframe',
-    'height': 90,
-    'width': 728,
-    'params': {}
-};
-document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/87a0b0107c646108dca671ff143edece/invoke.js"></scr' + 'ipt>');
+window.onload = function () {
+    atOptions = {
+        'key': '87a0b0107c646108dca671ff143edece',
+        'format': 'iframe',
+        'height': 90,
+        'width': 728,
+        'params': {}
+    };
+    document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/87a0b0107c646108dca671ff143edece/invoke.js"></scr' + 'ipt>');
+}
